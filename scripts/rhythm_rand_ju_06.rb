0a993cea5013d2b46499eed2e213a9678d0d47e0
komjum6 <jphh56@gmail.com>
2021-02-28 18:55:32 +0000

    Rand Rhythm samples added by Justin

---
+++ scripts/rhythm_rand_ju_06.rb
@@ -0,0 +1,46 @@
+#https://in-thread.sonic-pi.net/t/recording-sonic-pi-output-into-wav-files-programatically/949
+#https://in-thread.sonic-pi.net/t/recording-is-not-happening-with-osc-commands/4710
+
+#file naming convension
+feature = 'rhythm'
+type = 'rand'
+personID = 'ju'
+version = '06'
+
+#where to save
+#save_file = "D:/Jupyter_Projects/Jupyter Notebooks/SEM_2/Music/#{Time.now.strftime("%H-%F")}-melody#{version}ju.wav"
+save_file = "D:/Jupyter_Projects/Jupyter Notebooks/SEM_2/Music/#{feature}_#{type}_#{personID}_#{version}.wav"
+
+#what port are we using
+osc_send 'localhost',51235, '/start-recording','myGUID'
+
+#generate some sound to record
+in_thread(name: :one) do
+  20.times do
+    play 56
+    sleep rrand(0.2, 1.5)
+    play 68
+    sleep rrand(0.2, 1.5)
+    play 69
+    sleep rrand(0.2, 1.5)
+    play 60
+    sleep rrand(0.2, 1.5)
+    play 59
+    sleep rrand(0.2, 1.5)
+    play 55
+    sleep rrand(0.2, 1.5)
+    play 60
+    sleep rrand(0.2, 1.5)
+  end
+end
+
+#thread for saving
+in_thread(name: :save) do
+  #stop recording
+  sleep 14 # The maximum total duration
+  osc_send 'localhost',51235, '/stop-recording','myGUID'
+  sleep 0.5
+  #change the path in the next command to suit your system and usernames
+  osc_send 'localhost',51235, '/save-recording','myGUID', save_file
+end
+
