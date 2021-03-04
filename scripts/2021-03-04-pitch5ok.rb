233ae686d38b0f0a6e0d906a10d458ebff51702e
Okko98 <OkkoKruyssen@hotmail.nl>
2021-03-04 13:08:28 +0100

    Add files via upload

---
+++ scripts/2021-03-04-pitch5ok.rb
@@ -0,0 +1,61 @@
+#random sequence of tones in the c-toonladder without beginning with a c end ending with a c, representing
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 62, 64, 65, 67, 69, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
---
+++ scripts/2021-03-04-pitch7ok.rb
@@ -0,0 +1,61 @@
+#random sequence of tones in the c-toonladder without beginning with a c end ending with a c, representing
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.5
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
+play choose([60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]), cutoff: rrand(60, 120)
+sleep 0.25
