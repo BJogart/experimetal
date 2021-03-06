8a1f1a2671d4c2387e77b7df22fba06ff4e3a624
Okko98 <OkkoKruyssen@hotmail.nl>
2021-03-06 12:06:50 +0100

    Add files via upload

---
+++ pitch_recordings/pitch_Uniform3.rb
@@ -0,0 +1,50 @@
+# this script samples random notes based on probability distributions
+# distributions are conditioned on the previous note---it's basically
+# a hard-coded Markov chain.
+
+TONIC = :F4
+TEMPO = 0.75
+SEED = 3
+
+# the following are hardcoded probability distributions.
+# each distribution is a list of offsets from the tonic
+# a note is chosen uniformly from this list, so a note
+# that is repeated more often has a higher chance of
+# being chosen.
+
+# NB: these are offsets from the major scale(s); minor scales
+# are based off of different offsets. each distribution also
+# includes a special offset '13', which is not the 13th
+# note in the scale; it is instead interpreted as a single-note
+# rest. (the absence of a note.)
+
+# uniform distributions
+ant0 = [0, 2, 4, 5, 7, 9, 11, 13]
+ant2 = [0, 2, 4, 5, 7, 9, 11, 13]
+ant4 = [0, 2, 4, 5, 7, 9, 11, 13]
+ant5 = [0, 2, 4, 5, 7, 9, 11, 13]
+ant7 = [0, 2, 4, 5, 7, 9, 11, 13]
+ant9 = [0, 2, 4, 5, 7, 9, 11, 13]
+ant11 = [0, 2, 4, 5, 7, 9, 11, 13]
+antr = [0, 2, 4, 5, 7, 9, 11, 13]
+
+use_random_seed SEED
+
+define :play_random do |r, t|
+  n = ant0.choose if t == 0
+  n = ant2.choose if t == 2
+  n = ant4.choose if t == 4
+  n = ant5.choose if t == 5
+  n = ant7.choose if t == 7
+  n = ant9.choose if t == 9
+  n = ant11.choose if t == 11
+  n = antr.choose if t == 13
+  play r + n, release: TEMPO if n != 13
+  return n
+end
+
+n = 0
+loop do
+  n = play_random(TONIC, n)
+  sleep TEMPO
+end
