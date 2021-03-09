3d0f31f9ca8a99cf04d142b3c520ac4d1313449e
Okko98 <OkkoKruyssen@hotmail.nl>
2021-03-09 10:22:20 +0100

    Add files via upload

---
+++ final_scripts/P3R2_0.rb
@@ -0,0 +1,50 @@
+# this script samples random notes based on probability distributions
+# distributions are conditioned on the previous note---it's basically
+# a hard-coded Markov chain.
+
+
+TONIC = :C4
+TEMPO_0 = 0.25
+TEMPO_1 = 0.75
+SEED = 12
+
+# the following are hardcoded probability distributions.
+# each distribution is a list of offsets from the tonic
+# a note is chosen uniformly from this list, so a note
+# that is repeated more often has a higher chance of
+# being chosen.
+
+# NB: these are offsets from the major scale(s); minor scales
+# are based off of different offsets. each distribution also
+# includes a special offset '13', which is not the 13th
+# note in the scale; it is instead interpreted as a single-note
+# rest. (the absence of a note.)
+
+# Huron
+ant0 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,7,7,7,7,7,7,7,7,7,7,9,9,9,9,9,9,9,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11]
+ant2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,7,7,7,7,7,7,7,9,9,11,11,11,11,11]
+ant4 = [0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,9,9]
+ant5 = [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,9,9,9,9,9,11]
+ant7 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,9,9,9,9,9,9,9,9,9,9,9,9,11,11]
+ant9 = [0,0,0,0,2,2,2,4,5,5,5,5,5,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,11,11,11,11,11,11,11,11,11,11,11,11,11]
+ant11 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,4,5,7,7,7,7,7,7,7,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,11,11,11,11,11,11,11,11,11,11]
+use_random_seed SEED
+
+define :play_random do |r, t, tempo|
+  n = ant0.choose if t == 0
+  n = ant2.choose if t == 2
+  n = ant4.choose if t == 4
+  n = ant5.choose if t == 5
+  n = ant7.choose if t == 7
+  n = ant9.choose if t == 9
+  n = ant11.choose if t == 11
+  play r + n, release: tempo
+  sleep tempo
+  return n
+end
+
+n = 0
+loop do
+  n = play_random(TONIC, n, TEMPO_0)
+  n = play_random(TONIC, n, TEMPO_1)
+end
